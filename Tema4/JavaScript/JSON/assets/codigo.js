10f2910d1293ac0a2118df4ccf0020af001aad4d
adumitol <aroubel230@g.educaand.es>
2025-03-18 22:19:03 +0100

    final

--- Tema4/JavaScript/JSON/assets/codigo.js
+++ Tema4/JavaScript/JSON/assets/codigo.js
@@ -1,34 +1,78 @@
-// Seleccionamos el botón de búsqueda y añadimos un evento de clic que llama a la función buscarPokemon
-const buscadorPelicula = document.querySelector("#b1");
-buscadorPelicula.addEventListener("click", buscarPelicula);
+// constantes
+const tituloPelicula = document.querySelector(".Titulo");
+const anyoPelicula = document.querySelector(".Anyo");
+const duracionPelicula = document.querySelector(".Duracion");
+// valoraciones
+const valoraciones = document.querySelector("#seleccionValoraciones");
 
-// Función asíncrona para buscar información de un Pokémon
+// Constante del botón buscar
+const boton = document.querySelector("#b1");
+boton.addEventListener("click", buscarPelicula);
+
+// Función para buscar información de una película
 async function buscarPelicula() {
-    // Obtenemos el nombre del Pokémon ingresado por el usuario y construimos la URL para la API
-    const peliNombre = document.querySelector("#nombreBuscador").value.toLowerCase();
-    const url = `http://www.omdbapi.com/?t=${peliNombre}&apikey=6bd47da3`;
+    const pelicula = document.querySelector("#nombreBuscador").value.toLowerCase();
+    const url = `http://www.omdbapi.com/?t=${pelicula}&apikey=6bd47da3`;
 
-    // Seleccionamos los elementos del DOM donde se mostrará la información del Pokémon
-    const nombre = document.querySelector(".Titulo");
-    const Anyo = document.querySelector(".Anyo");
-    const Duracion = document.querySelector(".Duracion");
-    const respuesta = await fetch(url);
-    const datos = await respuesta.json();
+    try {
+        // Obtenemos la respuesta del URL
+        const response = await fetch(url);
+        // Procesamos la respuesta a un objeto data en JavaScript
+        const data = await response.json();
 
-        if (datos.Response == "False"){
-            // En caso de error, mostramos mensajes de "No encontrado" y limpiamos los datos mostrados
-            nombre.textContent = "No encontrado";
-            Duracion.textContent = "";
-            Anyo.textContent = "";
-            console.error("Error al buscar la Pelicula:", error);
-        }else{
-            
-            // Realizamos una solicitud a la API y obtenemos los datos del Pokémon
-        nombre.textContent = datos.Title;
-        Anyo.textContent = datos.Year;
-        Duracion.textContent = datos.Runtime;
+        // Verificamos si la película no fue encontrada
+        if (data.Response === "False") {
+            throw new Error("");
         }
-    } 
-        
-    
 
+        tituloPelicula.innerHTML = `<h3>${data.Title}</h3>`;
+        anyoPelicula.innerHTML = `<h3>${data.Year}</h3>`;
+        duracionPelicula.innerHTML = `<h3>${data.Runtime}</h3>`;
+
+        // Limpiamos antes para que no se repitan los tipos
+        valoraciones.innerHTML = "<option value=''>Selecciona una valoración</option>";
+
+        // Añadimos las nuevas valoraciones
+        data.Ratings.forEach(element => {
+            valoraciones.innerHTML += `<option>${element.Source}</option>`;
+        });
+
+    } catch (error) {
+        // Parrafo abajo del todo
+        resultado.innerHTML = `<p>No se encontró la película</p>`;
+    }
+}
+
+// Botón para buscar las opiniones
+const botonValoraciones = document.querySelector(".botonValoraciones");
+const resultado = document.querySelector(".resultadoValoraciones");
+
+// Evento para mostrar la valoración seleccionada
+botonValoraciones.addEventListener("click", mostrarValoracion);
+
+// Función para mostrar la valoración seleccionada
+async function mostrarValoracion() {
+    const pelicula = document.querySelector("#nombreBuscador").value.toLowerCase();
+    const url = `http://www.omdbapi.com/?t=${pelicula}&apikey=6bd47da3`;
+
+    try {
+        // Obtenemos la respuesta del URL
+        const response = await fetch(url);
+        const data = await response.json();
+
+        // Verificamos si la película no fue encontrada
+        if (data.Response === "False") {
+            throw new Error("");
+        }
+
+        // Añadimos las nuevas valoraciones
+        data.Ratings.forEach(element => {
+            if (element.Source == valoraciones.value) {
+                resultado.innerHTML = `<p>La valoración es: ${element.Value}</p>`;
+            }
+        });
+
+    } catch (error) {
+        resultado.innerHTML = `<p>No se encontró la opinión</p>`;
+    }
+}
